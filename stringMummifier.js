a363e54010dd5d0124f6686072207d3a986db5a4
Angeline <atan@thoughtworks.com>
2019-05-29 17:38:10 +0800

    Class solution for mummifier exercise

--- stringMummifier.js
+++ stringMummifier.js
@@ -1,17 +1,23 @@
 const VOWELS = ["a", "e", "i", "o", "u"];
 const REPLACEMENT = "mummy";
 
+//return same word if vowel is less than 30%
+// otherwuse replaceVowels???
 const mummify = word => {
+  if (typeof word !== "string") throw new TypeError("Please input string");
   if (isVowelLessThan30Percent(word)) {
     return word;
   }
   return replaceVowels(word);
 };
 
+//check if letter is vowel return boolean
 const isVowel = letter => {
   return VOWELS.includes(letter.toLowerCase());
 };
 
+//return true if word is empty str
+//return true if no of vowel in word is < 30%
 const isVowelLessThan30Percent = word => {
   let vowelCount = 0;
 
@@ -23,12 +29,15 @@ const isVowelLessThan30Percent = word => {
   return word.length === 0 || vowelCount / word.length < 0.3;
 };
 
+//
 const replaceVowels = word => {
   let mummified = "";
 
   for (let letter of word) {
+    //if not vowel concat to str
     if (!isVowel(letter)) {
       mummified += letter;
+      //str does not end with 'mummy' => concat 'mummy' to str
     } else if (!mummified.endsWith(REPLACEMENT)) {
       mummified += REPLACEMENT;
     }
--- stringMummifier.test.js
+++ stringMummifier.test.js
@@ -1,5 +1,43 @@
-const mummifiy = require("./stringMummifier")
+const mummifiy = require("./stringMummifier");
 
 describe("String Mummifier", () => {
-  
+  it("should modify singel vowel", () => {
+    expect(mummifiy("a")).toEqual("mummy");
+  });
+
+  it("should modify singe vowel capital letter", () => {
+    expect(mummifiy("A")).toEqual("mummy");
+  });
+
+  it("should not modify string with less than 30% vowels", () => {
+    expect(mummifiy("b")).toEqual("b");
+  });
+
+  it("should modify string with exactly 30% vowels", () => {
+    expect(mummifiy("hhhhhhhooo")).toEqual("hhhhhhhmummy");
+  });
+
+  it("should modify string with more than 30% vowels", () => {
+    expect(mummifiy("moon")).toEqual("mmummyn");
+  });
+
+  it("should return empty string if input empty string", () => {
+    expect(mummifiy("")).toEqual("");
+  });
+
+  test("should throw exception if input is not string", () => {
+    mummifiy(2);
+    const t = () => {
+      mummifiy(2);
+    };
+    expect(t).toThrow(TypeError);
+  });
+
+  it("should modify string twice with non consecutive vowels", () => {
+    expect(mummifiy("hello")).toEqual("hmummyllmummy");
+  });
+
+  it("should modify string once with consecutive vowels", () => {
+    expect(mummifiy("moon")).toEqual("mmummyn");
+  });
 });
